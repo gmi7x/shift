1d787b16f0bce13c63f9f07ceaa6c6282d9e2b03
gmi7x <gmi7x@users.noreply.github.com>
2017-02-16 14:59:07 +0100

    be verbose

--- sudo.sh
+++ sudo.sh
@@ -2,8 +2,12 @@
 
 if [ $EUID -ne 0 ]
 then
+    echo "adding user $EUID"
     echo "user:x:$EUID:0:the user:/tmp:/bin/bash" >> /etc/passwd
+    id
+    echo "running sudo -E '$@'"
     exec sudo -E "$@"
 else
+    echo "running '$@'"
     exec "$@"
 fi
